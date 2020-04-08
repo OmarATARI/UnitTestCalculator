fcfa23e4ee37ea2b4a36cc4c43cc53bdcd8824e8
ATARI Omar <atari.omar@gmail.com>
2020-04-08 15:45:45 +0200

    init

--- tests/CalculatorTest.php
+++ tests/CalculatorTest.php
@@ -7,23 +7,31 @@ use PHPUnit\Framework\TestCase;
 
 final class CalculatorTest extends TestCase
 {
-    public function testMul($a, $b)
+    public function testAdd($a, $b)
     {
-        self::assertEquals($a*$b, $this);
+        $calculator = new Calculator();
+        $result = $calculator->add(30, 12);
+        $this->assertEquals(42, $result);
     }
 
     public function testDiv($a, $b)
     {
-        self::assertEquals($a/$b, $this);
+        $calculator = new Calculator();
+        $result = $calculator->div(10, 2);
+        $this->assertEquals(5, $result);
     }
 
-    public function testAdd($a, $b)
+    public function testMul($a, $b)
     {
-        self::assertEquals($a+$b, $this);
+        $calculator = new Calculator();
+        $result = $calculator->mul(10, 2);
+        $this->assertEquals(20, $result);
     }
 
     public function testSub($a, $b)
     {
-        self::assertEquals($a-$b, $this);
+        $calculator = new Calculator();
+        $result = $calculator->sub(10, 2);
+        $this->assertEquals(8, $result);
     }
 }
